754d974e21ce40ef5adc50db082ff8618c6d163a
drunkduckdrown <70945319+drunkduckdrown@users.noreply.github.com>
2026-03-18 04:50:17 +0800

    Update model_updater.go

--- internal/registry/model_updater.go
+++ internal/registry/model_updater.go
@@ -20,7 +20,7 @@ const (
 )
 
 var modelsURLs = []string{
-	"https://raw.githubusercontent.com/drunkduckdrown/models/refs/heads/main/models.json",
+	"https://raw.githubusercontent.com/router-for-me/models/refs/heads/main/models.json",
 	"https://raw.githubusercontent.com/drunkduckdrown/models/refs/heads/main/models.json",
 }
 
